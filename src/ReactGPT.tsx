6b69ff4b1cb018a17a23c97a47b3e31ba19a7243
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-06-25 00:50:55 +0200

    implementacion de router

--- src/ReactGPT.tsx
+++ src/ReactGPT.tsx
@@ -1,6 +1,8 @@
+import { RouterProvider } from "react-router-dom"
+import { router } from "./presentation/router/router"
 
 export const ReactGPT = () => {
   return (
-    <h1 className="text-3xl font-bold underline">ReactGPT</h1>
+    <RouterProvider router={router} />
   )
 }
