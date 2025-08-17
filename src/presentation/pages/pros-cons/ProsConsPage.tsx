ee5404fac74b87d23144f2f2111da09140e1193a
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-08-17 13:31:15 +0200

    prosConsPage

--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,6 +1,62 @@
+import { useState } from "react";
+import { GptMessages, MyMessages, TextMessageBox, TypingLoader } from "../../components";
+
+interface Message {
+  text: string
+  isGpt: boolean
+}
 
 export const ProsConsPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    // TODO: useCase
+
+    setIsLoading(false);
+    // Todo: añadir el emensaje de isGPE en TRUE
+
+  }
+
   return (
-    <div>ProsConsPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessages text='Hola, puedes escribir lo que sea que quieras que compare o puntos de vista' />
+
+          {
+            messages.map((message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessages key={index} text="Esto es de OpenAI" />
+                ) : (
+                  <MyMessages key={index} text={message.text} />
+                )
+
+            ))
+          }
+
+          {
+            isLoading &&
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          }
+
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui lo que deseas"
+        disableCorrections
+      />
+
+    </div>
   )
 }
