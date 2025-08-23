2542efbce5292d65c936ffeb1d60fc1002ee8423
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-08-23 15:38:49 +0200

    stream response

---
+++ src/core/use-cases/pros-cons-stream.use-case.ts
@@ -0,0 +1,40 @@
+
+export const ProsConsStreamUseCase = async (prompt: string) => {
+    try {
+        const resp = await fetch(`${import.meta.env.VITE_GPT_API}/pros-cons-discusser-stream`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ prompt }),
+            // TODO: abortSIgnal
+        })
+
+        if (!resp.ok) throw new Error('No se pudo realizar la comparacion')
+
+        const reader = resp.body?.getReader();
+        if (!reader) {
+            console.log('no se pudo generar el reader');
+            return null;
+        }
+
+        const decoder = new TextDecoder();
+
+        let text = '';
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+                break;
+            };
+
+            const decodedChunk = decoder.decode(value, { stream: true });
+            text += decodedChunk;
+            console.log(text);
+        }
+
+    } catch (error) {
+        console.log('Hubo un error', error);
+
+        return null
+    }
+}
--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,6 +1,64 @@
+import { useState } from "react";
+import { GptMessages, MyMessages, TextMessageBox, TypingLoader } from "../../components";
+import { ProsConsStreamUseCase } from "../../../core/use-cases";
+
+
+interface Message {
+  text: string
+  isGpt: boolean
+}
 
 export const ProsConsStreamPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    // TODO: useCase
+    await ProsConsStreamUseCase(text)
+    setIsLoading(false);
+    // Todo: añadir el emensaje de isGPE en TRUE
+
+  }
+
   return (
-    <div>ProsConsStreamPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessages text='¿Que deseas comparar hoy?' />
+
+          {
+            messages.map((message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessages key={index} text="Esto es de OpenAI" />
+                ) : (
+                  <MyMessages key={index} text={message.text} />
+                )
+
+            ))
+          }
+
+          {
+            isLoading &&
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          }
+
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui lo que deseas"
+        disableCorrections
+      />
+
+    </div>
   )
 }
