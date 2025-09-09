8148e7a3e0111f0b6d416424938738466eea91c1
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-09-09 17:29:59 +0200

    call image generation

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,6 +1,70 @@
+import { useState } from "react"
+import { GptMessages, MyMessages, TypingLoader, TextMessageBox } from "../../components";
+import { imageGenerationUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+  info?: {
+    imageUrl: string;
+    alt: string;
+  }
+}
 
 export const ImageGenerationPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    const imageInfo = await imageGenerationUseCase(text)
+    setIsLoading(false);
+
+    if (!imageInfo) {
+      return setMessages((prev) => [...prev, { text: 'No se pudo generar la imagen', isGpt: true }])
+    }
+    // Todo: set messages
+
+  }
+
   return (
-    <div>ImageGenerationPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessages text='Hola, ¿Que imagen te gustaria generar?' />
+
+          {
+            messages.map((message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessages key={index} text="Esto es de OpenAI" />
+                ) : (
+                  <MyMessages key={index} text={message.text} />
+                )
+
+            ))
+          }
+
+          {
+            isLoading &&
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          }
+
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui lo que deseas"
+        disableCorrections
+      />
+
+    </div>
   )
 }
